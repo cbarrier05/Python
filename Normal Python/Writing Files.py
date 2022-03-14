3b6078ab78f2730977458028173de87291eb69db
cbarrier05 <cbarrier05@gmail.com>
2022-03-14 14:33:22 +0000

    School Work

---
+++ Normal Python/Writing Files.py
@@ -0,0 +1,15 @@
+# How to append to file
+#employee_file = open("ReadFilesExample.txt", "a")
+#employee_file.write("\nToby - Human Resources")  # Adds everytime the
+#employee_file.close()                           # Program run
+
+# How to write to file
+#employee_file = open("ReadFilesExample.txt", "w")
+#employee_file.write("Toby")  # Overwites entire file, only has Toby in it
+
+# How to make new file
+#employee_file = open("WriteFilesExample.txt", "w")
+#employee_file.write("Test")  # If "WireFilesExample.txt" doesn't exist
+#employee_file.close()   # it creates a new file with that name
+
+# Can write to any type of file, not just .txt
