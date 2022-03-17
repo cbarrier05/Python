52b1347739dc787a9b2d08133425871045fd27e9
cbarrier05 <cbarrier05@gmail.com>
2022-03-17 09:54:24 +0000

    Change

--- Python_ Tutorials/Normal Python/Reading Files.py
+++ Python_ Tutorials/Normal Python/Reading Files.py
@@ -4,13 +4,16 @@
                         # , "r+")    Allows you to both read and write
 
 employee_file = open("ReadFilesExample.txt", "r")
-
+employee_file.close()
 #print(employee_file.readable())   Returns boolean for whether it can be read
 #print(employee_file.read())       Outputs everything in the file
 #print(employee_file.readline())   Reads the first line
 #print(employee_file.readline())   Reads next line
 #print(employee_file.readlines())  Take all lines and put in list
 #print(employee_file.readlines()[1]) Outputs 2nd line in file
-for employee in employee_file.readlines():  # How to iterate through
-    print(employee)
-employee_file.close()   # Need to close once finished
+#for employee in employee_file.readlines():  # How to iterate through
+#    print(employee)
+#employee_file.close()   # Need to close once finished
+
+with open("ReadFilesExample.txt", "r") as file: # This auto closes the file when finished
+    print (file.readlines())
