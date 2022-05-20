caeffee8bbe71733b128d632b05d88a649c303e2
cbarrier05 <cbarrier05@gmail.com>
2022-05-20 14:26:01 +0100

    Button

---
+++ Python_ Tutorials/Tkinter/Buttons.py
@@ -0,0 +1,14 @@
+from tkinter import *
+root = Tk()
+
+def myClick():
+    myLabel1 = Label(root, text="Pressed")
+    myLabel1.pack()
+# How to create button       padx and pady change the size of the button
+myButton1 = Button(root, text="click me", padx=50)
+# How a state is used to disable a button
+myButton2 = Button(root, text="click me", state=DISABLED)
+# How to make function run when pressed  fg changes text colour  bg for background colour
+myButton3 = Button(root, text="click me", command=myClick, fg="blue")
+myButton3.pack()
+root.mainloop()
